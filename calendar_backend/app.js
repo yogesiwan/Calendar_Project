c1c657acc00096db28bd2648e93e81089174410d
yogesh <yogesh@yogeshs-MacBook-Air.local>
2024-10-27 18:48:01 +0530

    bug fixed

--- calendar_backend/app.js
+++ calendar_backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-    origin:   process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin:   process.env.CORS_ORIGIN,
     credentials: true, 
 }));
 
