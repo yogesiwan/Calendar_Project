bb6f6d5c13b1fd9c0674566a92e7e89dd181b35f
yogesh <yogesh@yogeshs-MacBook-Air.local>
2024-10-27 16:45:17 +0530

    changes

--- calendar_backend/config/mongoose-connection.js
+++ calendar_backend/config/mongoose-connection.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB successfully!');
+        console.log('server on render');
     })
     .catch((error) => {
         console.log('Error connecting to MongoDB:', error);
